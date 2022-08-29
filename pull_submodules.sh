359e30e5cc4f0c2f043d28069e450f374e453988
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2022-08-29 18:40:51 -0500

    Added docker composition, .gitmodules and pull_modules.sh, newsgears-api and newsgears-rss modules

---
+++ pull_submodules.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+
+# grab latest commits from server
+git submodule update --recursive --remote
+
+# above command will set current branch to detached HEAD. set back to master.
+git submodule foreach git checkout main
+
+# now do pull to fast-forward to latest commit
+git submodule foreach git pull origin main
